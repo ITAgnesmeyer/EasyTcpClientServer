cb11f03ae03be96e927c4c015adfe356680da9a0
DCSItoDIGA <guido.agnesmeyer@itagnesmeyer.com>
2021-07-23 13:05:36 +0200

    Test Clients

---
+++ ClientCLI/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using EasyTcpClientServer;
+namespace ClientCLI
+{
+    public class RequestProcessGa:RequestProcessBase 
+    {
+        public RequestProcessGa():base(true,"Hallo")
+        {
+            this.NextMessageToSend = "hallo";
+        }
+        protected override void Process(string message)
+        {
+            
+            switch (message)
+            {
+                case "aaa":
+                    //Console.WriteLine(@"Message:" + message);
+                    this.NextMessageToSend = "bbb";
+                    
+                    break;
+
+                case "bbb":
+                    this.NextMessageToSend = "aaa";
+                    
+                    break;
+                default:
+                    this.ReturnMessage = "";
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPClient client = new TCPClient("127.0.0.1", 12221);
+            RequestProcessGa req = new RequestProcessGa();
+            client.RegisterRequestProcess(req);
+            AttachEvents(client);
+            client.Start();
+            Console.WriteLine(@"Press any key to exit!");
+            string line;
+            do
+            {
+                line = Console.ReadLine();
+                req.NextMessageToSend =   line;
+                client.Send();
+            } while (line != "exit");
+
+            DetachEvents(client);
+            client.Stop();
+        }
+
+        static void AttachEvents(TCPClient server)
+        {
+            server.RequestProcessError += OnError;
+            server.RequestProcessSuccess += OnSuccess;
+        }
+
+
+        private static void OnSuccess(object sender, RequestProcessSuccessEventArgs e)
+        {
+            
+            Console.WriteLine(@"Success:" + e.Message);
+        }
+
+        static void DetachEvents(TCPClient server)
+        {
+            server.RequestProcessError -= OnError;
+            server.RequestProcessSuccess -= OnSuccess;
+
+        }
+
+        private static void OnError(object sender, RequestProcessErrorEventArgs e)
+        {
+            if (sender == null && e.ClientMessage.StartsWith("-!"))
+            {  }
+            else
+                Console.WriteLine(@"Message:" + e.ClientMessage + @"Exception:" + e.ExceptionMessage);
+        }
+    }
+}
---
+++ ServerCLI/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyTcpClientServer;
+namespace ServerCLI
+{
+    public class RequstProcessGa:RequestProcessBase 
+    {
+        public RequstProcessGa():base(true,"Hallo")
+        {
+            
+        }
+        protected override void Process(string message)
+        {
+            switch (message)
+            {
+                case "aaa":
+                    Console.WriteLine("Message:" + message);
+                    this.ReturnMessage = "Alles ist gut";
+                    break;
+
+                case "bbb":
+                    this.ReturnMessage = "Nicht implementiert";
+                    break;
+                case "ccc":
+                    this.ReturnMessage = "aaa";
+                    break;
+                case "ddd":
+                    this.ReturnMessage = "bbb";
+                    break;
+                default:
+                    this.ReturnMessage = " ";
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPServer server = new TCPServer("127.0.0.1", 12221);
+            RequstProcessGa req = new RequstProcessGa();
+            server.RegisterRequestProcess(req);
+
+            AttachEvents(server);
+            server.Start();
+
+            Console.WriteLine("Press any key to exit!");
+
+            Console.ReadKey();
+            DetachEvents(server);
+
+            server.Stop();
+        }
+
+        static void AttachEvents(TCPServer server)
+        {
+            server.RequestProcessError += OnError;
+            server.RequestProcessSuccess += OnSuccess;
+        }
+
+
+        private static void OnSuccess(object sender, RequestProcessSuccessEventArgs e)
+        {
+            Console.WriteLine("Success:" + e.Message);
+        }
+
+        static void DetachEvents(TCPServer server)
+        {
+            server.RequestProcessError -= OnError;
+            server.RequestProcessSuccess -= OnSuccess;
+
+        }
+
+        private static void OnError(object sender, RequestProcessErrorEventArgs e)
+        {
+            if (sender == null && e.ClientMessage.StartsWith("-!"))
+            {  }
+            else
+                Console.WriteLine("Message:" + e.ClientMessage + "Exception:" + e.ExceptionMessage);
+        }
+    }
+}
